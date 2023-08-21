8f4bedfe50a9fcc72d4ccb5c0035f41b72aac9b8
AneeahBaby <142260744+AneeahBaby@users.noreply.github.com>
2023-08-21 13:24:03 -0500

    Started JS structure

---
+++ style.js
@@ -0,0 +1,4 @@
+let apikey="b344b5f6tf2co3e462662ea6f4a0400c";
+let apiURL = `https://api.shecodes.io/weather/v1/current?query=Chicago&key={b344b5f6tf2co3e462662ea6f4a0400c}`;
+
+console.log(apiURL);
