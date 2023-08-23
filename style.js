cf97ef7efd79cb7c4210bfa8851d397647e11af2
AneeahBaby <142260744+AneeahBaby@users.noreply.github.com>
2023-08-22 20:32:49 -0500

    Updated js

--- style.js
+++ style.js
@@ -1,4 +1,11 @@
-let apikey = "ca0db41e2e878c74a1dfc7ffece370d4";
-let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=${apikey}&units=metric`;
 
-console.log(apiURL);
+function displayTemperature(response) {
+  console.log(response.data);
+}
+  let city="Chicago"
+  let apiKey = "ca0db41e2e878c74a1dfc7ffece370d4";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+
+
