fc888592c6c518bcc8c9856bee6afed982d81235
AneeahBaby <142260744+AneeahBaby@users.noreply.github.com>
2023-08-28 13:52:24 -0500

    Added some functions to js

--- style.js
+++ style.js
@@ -1,6 +1,13 @@
 
 function displayTemperature(response) {
   console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
 }
   let city="Chicago"
   let apiKey = "ca0db41e2e878c74a1dfc7ffece370d4";
