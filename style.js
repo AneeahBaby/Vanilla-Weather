d4d24a0849cce0c173c70e21bd5055edf3d02ebc
AneeahBaby <142260744+AneeahBaby@users.noreply.github.com>
2023-09-26 15:59:21 -0500

    final touches in CSS & js

--- style.js
+++ style.js
@@ -53,7 +53,7 @@ function formatDate(timestamp) {
         function getForecast(coordinates) {
             console.log(coordinates);
             let apiKey = "ca0db41e2e878c74a1dfc7ffece370d4";
-            let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+            let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
             axios.get(apiUrl).then(displayForecast);
         }
 
@@ -83,7 +83,7 @@ function displayTemperature(response) {
 
 function search(city) {
     let apiKey = "ca0db41e2e878c74a1dfc7ffece370d4";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
     axios.get(apiUrl).then(displayTemperature);
 
 }
@@ -93,30 +93,10 @@ function handlesearch(event) {
     search(cityNameElement.value);
 }
 
-function displayFahrenheitTemp(event) {
-    event.preventDefault();
-    let temperatureElement = document.querySelector("#temperature");
-    let fahrenheitTemp = (celciusTemp * 9) / 5 + 32;
-    temperatureElement.innerHTML = Math.round(fahrenheitTemp);
-}
-
-function displayCelciusTemp(event) {
-    event.preventDefault();
-    let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = Math.round(celciusTemp);
-}
-
-let celciusTemp = null;
-
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handlesearch);
 
-let fahrenheitlink = document.querySelector("#fahrenheit-link");
-fahrenheitlink.addEventListener("click", displayFahrenheitTemp);
-
-let celciuslink = document.querySelector("#celcius-link");
-celciuslink.addEventListener("click", displayCelciusTemp);
 
 
 search("Chicago");
