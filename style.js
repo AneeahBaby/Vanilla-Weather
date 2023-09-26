bbf8c0760f034b64533de9364ecc86081a8b0ed8
AneeahBaby <142260744+AneeahBaby@users.noreply.github.com>
2023-09-26 12:19:57 -0500

    finished JS for weather app

--- style.js
+++ style.js
@@ -13,29 +13,48 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
-    let forecastElement = document.querySelector("#forecast");
-     let days= ["Thu", "Fri", "Sat", "Sun"];
+    function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days=["Sun","Mon","Tues","Wed","Thurs","Fri","Sat"]
+
+    return days[day];
+    }
+     
+
+     function displayForecast(response) {
+     let forecast= response.data.daily;
+     let forecastElement = document.querySelector("#forecast");
+     
 
     let forecastHTML = `<div class="row">`;
 
-    days.forEach(function (day) {
-   
+       forecast.forEach(function (forecastday, index) {
+        if (index < 6) {
       forecastHTML =
-      forecastHTML +
-      `                 
+        forecastHTML +
+        `                 
         <div class="col-2">
-          <div class="weather-forecast-date">${day}</div>
-             <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="" width="42" />
-                <span class="weather-forecast-temp-max"> 18º </span>
-                <span class="weather-forecast-temp-min"> 12º </span>
+          <div class="weather-forecast-date">${formatDay(forecastday.dt)}</div>
+          
+             <img src="http://openweathermap.org/img/wn/${forecastday.weather[0].icon}@2x.png" alt="" width="42" />
+                <span class="weather-forecast-temp-max">${Math.round(forecastday.temp.max)}º </span>
+                <span class="weather-forecast-temp-min"> ${Math.round(forecastday.temp.min)}º </span>
                     </div>
                     
                 `;
+          }
           });
                 forecastHTML = forecastHTML + `</div>`;
                  forecastElement.innerHTML = forecastHTML;
 
+        }
+
+        function getForecast(coordinates) {
+            console.log(coordinates);
+            let apiKey = "ca0db41e2e878c74a1dfc7ffece370d4";
+            let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+            axios.get(apiUrl).then(displayForecast);
         }
 
 function displayTemperature(response) {
@@ -57,8 +76,11 @@ function displayTemperature(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+
+  getForecast(response.data.coord);
 }
  
+
 function search(city) {
     let apiKey = "ca0db41e2e878c74a1dfc7ffece370d4";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -98,4 +120,4 @@ celciuslink.addEventListener("click", displayCelciusTemp);
 
 
 search("Chicago");
-displayForecast();
+
